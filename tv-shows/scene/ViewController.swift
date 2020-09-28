25791006581ef92ee25e770e1bef444044703b9c
Guilherme Colombini <guilherme.colombini@ab-inbev.com>
2020-09-28 16:32:51 -0300

    setup project

---
+++ tv-shows/scene/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  tv-shows
+//
+//  Created by Guilherme Colombini on 28/09/20.
+//  Copyright © 2020 Guilherme Colombini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
